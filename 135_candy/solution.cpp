8254095adedfd8f4f76cc922383e0db43d34bac8
Liad Firouz <liad.firouz@gmail.com>
2026-03-11 22:48:21 +0200

    135 done.

--- 135_candy/solution.cpp
+++ 135_candy/solution.cpp
@@ -19,5 +19,20 @@ public:
             else
                 candies[i] = MIN_CANDY;
         }
+
+        int i = ratings.size() - 2;
+        while (i >= 0)
+        {
+            if (ratings[i] > ratings[i + 1])
+                candies[i--] = max(candies[i], candies[i + 1] + 1);
+        }
+
+        int totalCandies = 0;
+        for (int c : candies)
+        {
+            totalCandies += c;
+        }
+
+        return totalCandies;
     }
 };
